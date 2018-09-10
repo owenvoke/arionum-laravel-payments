d7963f33f50f2ad452617f78ee60972cdf7b55a2
pxgamer <owzie123@gmail.com>
2018-09-10 14:28:25 +0100

    Add initial service provider

---
+++ src/ArionumProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace pxgamer\ArionumLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ArionumProvider
+ */
+class ArionumProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
