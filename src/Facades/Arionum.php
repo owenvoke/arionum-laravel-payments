aacbef731bbc835854dc1dda1c8cd9e9f8a1b18a
pxgamer <owzie123@gmail.com>
2018-10-09 14:45:40 +0100

    Add type hints for currently available methods

--- src/Facades/Arionum.php
+++ src/Facades/Arionum.php
@@ -7,6 +7,25 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class Arionum
  *
+ * @method static string getAddress(string $publicKey)
+ * @method static string getBase58(string $data)
+ * @method static string getBalance(string $address)
+ * @method static string getPendingBalance(string $address)
+ * @method static string getTransactions(string $address)
+ * @method static \stdClass getTransaction(string $transactionId)
+ * @method static string getPublicKey(string $address)
+ * @method static \stdClass generateAccount()
+ * @method static \stdClass getCurrentBlock()
+ * @method static \stdClass getBlock(int $height)
+ * @method static array getBlockTransactions(string $blockId)
+ * @method static string getNodeVersion()
+ * @method static string sendTransaction(\pxgamer\Arionum\Transaction $transaction)
+ * @method static int getMempoolSize()
+ * @method static int getRandomNumber(int $height, int $minimum, int $maximum, string $seed = null)
+ * @method static array getMasternodes()
+ * @method static string getAlias(string $address)
+ * @method static string getNodeAddress()
+ *
  * @see \pxgamer\Arionum\Arionum
  */
 class Arionum extends Facade
